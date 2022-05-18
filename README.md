a7f801eef7a2af9c963d7cab36787cfb8d690909
EC2 Default User <105344687+Alexander4649@users.noreply.github.com>
2022-05-18 08:23:06 +0000

    [Add]ルーティング記述,新規投稿,form_with補足

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # README
 
+[Add] 初期設定完了
+
+[Start]...開始
+[Finish]...終了
+[Add]...追加
+[Update]...更新
+[Remove]...削除
+[Fix]...不具合修正
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  post 'lists'=>'lists#create'
+  post 'lists'=>'lists#create' #コントローラにフォームのデータを送信する際にHTTPメソッドである[POST(データの新規保存)]を使用する
   get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
