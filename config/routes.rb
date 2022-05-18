67cd53a7d3360ab877ed825ba765d5a26e92665d
EC2 Default User <105344687+Alexander4649@users.noreply.github.com>
2022-05-18 08:57:18 +0000

    [Add]ルーティング設定、[Finish]一覧画面表示しよう

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  get '/top' => 'homes#top'
   post 'lists'=>'lists#create' #コントローラにフォームのデータを送信する際にHTTPメソッドである[POST(データの新規保存)]を使用する
-  get 'lists/index'
+  get 'lists'=>'lists#index' #作成したデータを表示するため
   get 'lists/show'
   get 'lists/edit'
-  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
