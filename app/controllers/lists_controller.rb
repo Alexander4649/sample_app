9b90def0f65fd745b639793668425685d24108d0
EC2 Default User <105344687+Alexander4649@users.noreply.github.com>
2022-05-18 08:00:36 +0000

    保存機能を追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,19 @@
 class ListsController < ApplicationController
+  
   def new 
     #Viweへデータを渡す為のインスタンス変数に空のModelオブジェクトを生成する。
     @list = List.new
+  end
+  
+  def create
+    #1.&2. データを受け取り新規登録をするためのインスタンス作成
+    #list変数にはList(Model)のメソッドであるnewメソッドが呼び出され、引数list_paramsが呼び出される。
+    #保存機能の為、ローカル変数を利用している。
+    list = List.new(list_params)
+    #3. 呼び出したModelインスタンスデータをデータベースに保存するためのsaveメゾット実行
+    list.save
+    #4. トップ画面へリダイレクト
+    redirect_to'/top'
   end
 
   def index
@@ -11,5 +23,12 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+  
+  #privateとは一種の境界線で「ここから下はこのcontrollerの中でしか呼び出せません」という意味がある。
+  private
+  #ストロングパラメータ...マスアサインメント脆弱性を防ぐパラメータ
+  def list_params
+    params.require(:list).permit(:title,:body)
   end
 end
