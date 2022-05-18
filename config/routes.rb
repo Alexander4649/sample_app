9485d478e8ab3017a052cec08bb063e0c3ed5ad6
EC2 Default User <105344687+Alexander4649@users.noreply.github.com>
2022-05-18 10:18:11 +0000

    [Add] 詳細画面-名前付ルーティング、link_toの変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get '/top' => 'homes#top'
   post 'lists'=>'lists#create' #コントローラにフォームのデータを送信する際にHTTPメソッドである[POST(データの新規保存)]を使用する
   get 'lists'=>'lists#index' #作成した一覧表示データを表示するため
-  get 'lists/:id' => 'lists#show' #詳細画面はどの投稿データを表示させるか判別する為、投稿データのidもURLに含めます
+  get 'lists/:id' => 'lists#show',as:'list' #詳細画面はどの投稿データを表示させるか判別する為、投稿データのidもURLに含めます
                                   #詳細画面で呼び出される投稿データはURLの/lists/:idで判別します。 :idはアクション内にparams[:id]で取得。
+                                  # as: オプションを追加することで'lists#show'の設定を'list'として利用するという
   get 'lists/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
