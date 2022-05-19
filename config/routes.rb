657048e4cf62c2aa15b8c862afe532dc482d315b
Alexander4649 <105344687+Alexander4649@users.noreply.github.com>
2022-05-19 03:38:27 +0000

    [Finish]削除機能を作ろう

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'lists/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #resources :lists
   get '/top' => 'homes#top'
   post 'lists'=>'lists#create' #コントローラにフォームのデータを送信する際にHTTPメソッドである[POST(データの新規保存)]を使用する
   get 'lists'=>'lists#index' #作成した一覧表示データを表示するため
