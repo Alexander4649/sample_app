5a2f1d0813d6d835fa5f71b4548920b523fd1410
Alexander4649 <105344687+Alexander4649@users.noreply.github.com>
2022-05-19 01:32:46 +0000

    [Add]更新機能を追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,19 +1,15 @@
 class ListsController < ApplicationController
   
   def new #新規投稿していく為のアクション
-    #Viweへデータを渡す為のインスタンス変数に空のModelオブジェクトを生成する。
-    @list = List.new
+    @list = List.new  #Viweへデータを渡す為のインスタンス変数に空のModelオブジェクトを生成する。
   end
   
   def create
-    #1.&2. データを受け取り新規登録をするためのインスタンス作成
-    #list変数にはList(Model)のメソッドであるnewメソッドが呼び出され、引数list_paramsが呼び出される。
-    #保存機能の為、ローカル変数を利用している。
-    list = List.new(list_params)
-    #3. 呼び出したModelインスタンスデータをデータベースに保存するためのsaveメゾット実行
-    list.save
-    #4. トップ画面へリダイレクト
-    redirect_to list_path(list.id)
+    list = List.new(list_params)   #1.&2. データを受け取り新規登録をするためのインスタンス作成
+                                   # list変数にはList(Model)のメソッドであるnewメソッドが呼び出され、引数list_paramsが呼び出される。
+                                   # 保存機能の為、ローカル変数を利用している。
+    list.save                      #3. 呼び出したModelインスタンスデータをデータベースに保存するためのsaveメゾット実行
+    redirect_to list_path(list.id) #4. トップ画面へリダイレクト
   end
 
   def index # 一覧表示アクション、今回は一覧画面に投稿したList全てを表示させるので全データを取得する
@@ -29,9 +25,14 @@ class ListsController < ApplicationController
     @list = List.find(params[:id]) #投稿済みのデータを編集するので保存されているデータを見つける為にfindメソッドを使用する。
   end
   
-  #privateとは一種の境界線で「ここから下はこのcontrollerの中でしか呼び出せません」という意味がある。
-  private
-  #ストロングパラメータ...マスアサインメント脆弱性を防ぐパラメータ
+  def update
+    list = List.find(params[:id])  # 更新機能の為のアクション、updateアクションを作成する。
+    list.update(list_params)       # showアクションにリダイレクトする為に、引数には必ずidが必要になります。
+    redirect_to list_path(list.id) # idを付ける事で、どのデータを詳細画面で表示させるかを決定しています。
+  end
+  
+  private #privateとは一種の境界線で「ここから下はこのcontrollerの中でしか呼び出せません」という意味がある。
+          #ストロングパラメータ...マスアサインメント脆弱性を防ぐパラメータ
   def list_params
     params.require(:list).permit(:title,:body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/top' => 'homes#top'
   post 'lists'=>'lists#create' #コントローラにフォームのデータを送信する際にHTTPメソッドである[POST(データの新規保存)]を使用する
   get 'lists'=>'lists#index' #作成した一覧表示データを表示するため
@@ -7,5 +8,6 @@ Rails.application.routes.draw do
                                   #詳細画面で呼び出される投稿データはURLの/lists/:idで判別します。 :idはアクション内にparams[:id]で取得。
                                   # as: オプションを追加することで'lists#show'の設定を'list'として利用するという
   get 'lists/:id/edit' => 'lists#edit',as: 'edit_list' #ルーティングのURLに:idを記述すると、lists/●●/editの全てのURLが対象になる。
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  patch 'lists/:id' => 'lists#update', as: 'update_list' #更新の場合のHTTPメソッドはPTACHで指定します。
+
 end
